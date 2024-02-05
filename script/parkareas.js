a0635f5e1575bdc49e796a4bdc042c096060dcb2
Kelsey Mcnutt <kelseymcnutt@Kelseys-Air.home>
2024-02-05 15:17:47 -0600

    created functions for parks

--- script/parkareas.js
+++ script/parkareas.js
@@ -1,9 +1,37 @@
-/*function to display parkArea
+import {getParks, getGuests} from './script/database.js'
 
-functionName: displayPark
+const parks = getParks()
+const guests = getGuests()
+const displayPark = () => {
+let parksHTML = '<section class="park-card">'
+for(const park of parks) {
+    parksHTML += `<div>
+    <h1 data-type="park" data-id="${park.id}">${park.name}</h1>
+    <div>
+    <div>${displayServices(park.id)}</div>`
+    }
+    parksHTML += '</section>'
+    return parksHTML
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        const parkId = itemClicked.dataset.id
+        if (itemClicked.dataset.type === "park") {
+            let guestCounter = 0
+            for (const guest of guests) {
+                if (parseInt(parkId) === guest.parkAreaId) {
+                    guestCounter++
+                }
+            }
+            window.alert(`There are ${guestCounter} guests in this are`)
+        }
+    }
+)
 
 
 
 
-add event listener to display how many guests are at the park area currently 
-*/
+
